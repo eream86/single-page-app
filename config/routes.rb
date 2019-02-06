862a1644b303c40e42cef78dca4741789f13ad21
Eric Ream <Ream@Jordans-Air.localdomain>
2019-02-06 14:06:23 -0800

    adds authenticated routes and unauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :dvds, constraints: ->(request){ !request.format.html? }
   devise_for :users
-    get 'protected', to: 'pages#protected'
-    get 'unprotected', to: 'pages#unprotected'
+
+    get '*path', to: 'pages#protected', constraints: ->(request){ request.format.html? }
+    root to: 'pages#unprotected'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_184543) do
+ActiveRecord::Schema.define(version: 2019_02_06_211605) do
+
+  create_table "dvds", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
